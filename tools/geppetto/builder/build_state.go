173498f387851dd41d6266c74d156cb7a654888c
Ben Wright <benjamin.p.wright@hotmail.com>
2020-08-29 14:01:55 +0100

    adding build state

--- tools/geppetto/builder/build_state.go
+++ tools/geppetto/builder/build_state.go
@@ -6,28 +6,28 @@ import (
 	"github.com/sirupsen/logrus"
 )
 
-type toyOrder struct {
-	toy   string
+type repoState struct {
+	repo  string
 	state State
 }
 
 // BuildState represents the current state of all the builds
 type BuildState struct {
-	orders []toyOrder
+	orders []repoState
 }
 
 // NewBuildState creates a new empty build state
 func NewBuildState() BuildState {
-	return BuildState{orders: []toyOrder{}}
+	return BuildState{orders: []repoState{}}
 }
 
-func (s *BuildState) add(toy string, state State) {
-	s.orders = append(s.orders, toyOrder{toy: toy, state: state})
+func (s *BuildState) add(repo string, state State) {
+	s.orders = append(s.orders, repoState{repo: repo, state: state})
 }
 
-func (s *BuildState) find(toy string) *toyOrder {
+func (s *BuildState) find(repo string) *repoState {
 	for index, order := range s.orders {
-		if order.toy == toy {
+		if order.repo == repo {
 			return &s.orders[index]
 		}
 	}
@@ -35,25 +35,25 @@ func (s *BuildState) find(toy string) *toyOrder {
 	return nil
 }
 
-func (s *BuildState) setEndState(toy string, state State) error {
-	existing := s.find(toy)
+func (s *BuildState) setEndState(repo string, state State) error {
+	existing := s.find(repo)
 
 	if existing == nil {
-		s.add(toy, state)
+		s.add(repo, state)
 		return nil
 	}
 
 	if existing.state != BuildingState {
-		return fmt.Errorf("Cannot put toy %s into %s state when already in %s", toy, state, existing.state)
+		return fmt.Errorf("Cannot put repo %s into %s state when already in %s", repo, state, existing.state)
 	}
 
 	existing.state = state
 	return nil
 }
 
-// GetState returns the build state for the given toy
-func (s *BuildState) GetState(toy string) State {
-	order := s.find(toy)
+// GetState returns the build state for the given repo
+func (s *BuildState) GetState(repo string) State {
+	order := s.find(repo)
 	if order != nil {
 		return order.state
 	}
@@ -61,44 +61,44 @@ func (s *BuildState) GetState(toy string) State {
 	return NoneState
 }
 
-// Build signal that a toy is being built
-func (s *BuildState) Build(toy string) error {
-	state := s.GetState(toy)
+// Build signal that a repo is being built
+func (s *BuildState) Build(repo string) error {
+	state := s.GetState(repo)
 	if state != NoneState {
-		return fmt.Errorf("Cannot put toy %s into building state when already in %s", toy, state)
+		return fmt.Errorf("Cannot put repo %s into building state when already in %s", repo, state)
 	}
 
-	s.add(toy, BuildingState)
+	s.add(repo, BuildingState)
 	return nil
 }
 
-// Complete signal that a toy build has finished
-func (s *BuildState) Complete(toy string) error {
-	return s.setEndState(toy, BuiltState)
+// Complete signal that a repo build has finished
+func (s *BuildState) Complete(repo string) error {
+	return s.setEndState(repo, BuiltState)
 }
 
-// Block signal that a toy build has been blocked by a dependency build failure
-func (s *BuildState) Block(toy string) error {
-	return s.setEndState(toy, BlockedState)
+// Block signal that a repo build has been blocked by a dependency build failure
+func (s *BuildState) Block(repo string) error {
+	return s.setEndState(repo, BlockedState)
 }
 
-// Error signal that a toy build has failed
-func (s *BuildState) Error(toy string) error {
-	return s.setEndState(toy, ErroredState)
+// Error signal that a repo build has failed
+func (s *BuildState) Error(repo string) error {
+	return s.setEndState(repo, ErroredState)
 }
 
-// CanBuild returns true if the toy itself is not in a building, built, errored or blocked state
-func (s *BuildState) CanBuild(toy string, deps []string) (ok bool, block bool) {
-	state := s.GetState(toy)
+// CanBuild returns true if the repo itself is not in a building, built, errored or blocked state
+func (s *BuildState) CanBuild(repo string, deps []string) (ok bool, block bool) {
+	state := s.GetState(repo)
 
 	if state != NoneState {
-		logrus.Debugf("Not building %s because state is %s", toy, state)
+		logrus.Debugf("Not building %s because state is %s", repo, state)
 		return false, false
 	}
 
 	for _, dep := range deps {
 		if state := s.GetState(dep); state != BuiltState {
-			logrus.Debugf("Not building %s because dep %s state is %s", toy, dep, state)
+			logrus.Debugf("Not building %s because dep %s state is %s", repo, dep, state)
 			return false, state == ErroredState || state == BlockedState
 		}
 	}
