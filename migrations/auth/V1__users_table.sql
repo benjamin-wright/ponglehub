06f9c05e85dc5917832ee5e2db0189de8806ba2b
Ben Wright <benjamin.wright@nielsen.com>
2020-10-12 16:35:03 +0100

    working on table schemas

--- migrations/auth/V1__users_table.sql
+++ migrations/auth/V1__users_table.sql
@@ -1,4 +1,29 @@
-create table PERSON (
-    ID int not null,
-    NAME varchar(100) not null
+drop table if exists users;
+drop table if exists used_access_tokens;
+drop table if exists user_refresh_tokens;
+
+create table users (
+    id uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
+    name varchar(100) not null,
+    email varchar(100) not null,
+    password varchar(100) not null,
+    verified boolean not null,
+);
+
+create table used_access_tokens (
+    id uuid not null PRIMARY KEY,
+    user_id uuid not null,
+    CONSTRAINT fk_user
+        FOREIGN KEY(user_id)
+            REFERENCES users(id)
+            ON DELETE CASCADE
+);
+
+create table used_refresh_tokens (
+    id uuid not null PRIMARY KEY,
+    user_id uuid not null,
+    CONSTRAINT fk_user
+        FOREIGN KEY(user_id)
+            REFERENCES users(id)
+            ON DELETE CASCADE
 );
