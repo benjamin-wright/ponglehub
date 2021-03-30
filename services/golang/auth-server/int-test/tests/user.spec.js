791132c69e0d7de082de9dd3cf98757f06938ac7
Ben Wright <benjamin.p.wright@hotmail.com>
2021-03-30 20:17:57 +0100

    more tests

--- services/golang/auth-server/int-test/tests/user.spec.js
+++ services/golang/auth-server/int-test/tests/user.spec.js
@@ -11,12 +11,20 @@ async function listUsers() {
 }
 
 async function deleteUser(username) {
-    console.log(`Deleting user: ${username}`);
     const result = await axios.delete(`http://auth-server/user/${username}`);
 
     if (result.status !== 202) {
         throw new Error(`Failed deleting user: ${result.status}`);
     }
+}
+
+async function expectUsers(users) {
+    await expect(
+        axios.get('http://auth-server/user').then(res => ({ status: res.status, data: res.data }))
+    ).resolves.toEqual({
+        status: 200,
+        data: users
+    });
 }
 
 describe('users route', () => {
@@ -26,20 +34,51 @@ describe('users route', () => {
     });
 
     describe('get', () => {
+        beforeEach(async () => {
+            await expect(
+                axios.post('http://auth-server/user', { name: 'test-user', password: 'pwd', email: 'user@notathing.com' }).then(res => ({ status: res.status }))
+            ).resolves.toEqual({
+                status: 202
+            });
+        });
+
         it('should return 404 if user doesn\'t exist', async () => {
             await expect(
                 axios.get('http://auth-server/user/something')
             ).rejects.toMatchError('Request failed with status code 404');
         });
+
+        it('should return the user if it does exist', async () => {
+            await expect(
+                axios.get('http://auth-server/user/test-user').then(res => ({ status: res.status, data: res.data }))
+            ).resolves.toEqual({
+                status: 200,
+                data: {
+                    id: expect.any(String),
+                    name: 'test-user',
+                    email: 'user@notathing.com',
+                    verified: false
+                }
+            });
+        });
     });
 
     describe('post', () => {
-        it('should return a dummy ok code', async () => {
+        it('should add the user to the database', async () => {
             await expect(
                 axios.post('http://auth-server/user', { name: 'random', password: 'pwd', email: 'user@notathing.com' }).then(res => ({ status: res.status }))
             ).resolves.toEqual({
                 status: 202
             });
+
+            await expectUsers([
+                {
+                    id: expect.any(String),
+                    name: 'random',
+                    email: 'user@notathing.com',
+                    verified: false
+                }
+            ]);
         });
     });
 
