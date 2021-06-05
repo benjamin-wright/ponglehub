d966eac53d2fba0c7cbfbaeae017cbe85bd1aa65
Ben Wright <benjamin.p.wright@hotmail.com>
2021-06-05 17:41:43 +0100

    getting tilt and earthly builds working

--- services/auth-operator/src/Main.rs
+++ services/auth-operator/src/Main.rs
@@ -1,4 +1,17 @@
+use tokio::time::sleep;
+use std::time::Duration;
+use log::{info};
+use anyhow::anyhow;
+
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
-    Ok(())
+    env_logger::init();
+    info!("Starting...");
+    
+    loop {
+        info!("yofo!");
+        sleep(Duration::from_secs(2)).await
+    }
+
+    Err(anyhow!("them"))
 }
