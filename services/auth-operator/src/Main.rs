8b2ac7a9e7d364ffd19ead906e778378874a338d
Ben Wright <benjamin.p.wright@hotmail.com>
2021-06-11 13:08:08 +0100

    getting operator rbac sorted

--- services/auth-operator/src/Main.rs
+++ services/auth-operator/src/Main.rs
@@ -1,17 +1,17 @@
-use tokio::time::sleep;
-use std::time::Duration;
+use reflector::store::Writer;
 use log::{info};
-use anyhow::anyhow;
+use futures::{StreamExt, TryStreamExt};
 use serde::{Deserialize, Serialize};
 use schemars::JsonSchema;
 use kube::{
     api::{Api, ListParams, ResourceExt},
     Client, CustomResource,
 };
+use kube_runtime::{reflector, utils::try_flatten_applied, watcher};
 
 #[derive(CustomResource, Deserialize, Serialize, Clone, Debug, JsonSchema)]
-#[kube(group = "auth.ponglehub.co.uk", version = "v1beta1", kind = "User")]
-pub struct UserSpec {
+#[kube(group = "auth.ponglehub.co.uk", version = "v1beta1", kind = "AuthUser", plural="auth-users", singular="auth-user")]
+pub struct AuthUserSpec {
     name: String,
     email: String,
     password: String
@@ -25,10 +25,28 @@ async fn main() -> anyhow::Result<()> {
     let client = Client::try_default().await?;
     info!("Got client");
 
-    loop {
-        info!("hey ho!");
-        sleep(Duration::from_secs(10)).await
+    let namespace = std::env::var("NAMESPACE").unwrap_or("default".into());
+
+    let store = Writer::<AuthUser>::default();
+    let reader = store.as_reader();
+    let users: Api<AuthUser> = Api::namespaced(client, &namespace);
+
+    let lp = ListParams::default().timeout(60);
+    let rf = reflector(store, watcher(users, lp));
+
+    tokio::spawn(async move {
+        loop {
+            // Periodically read our state
+            tokio::time::sleep(std::time::Duration::from_secs(60)).await;
+            let crds = reader.state().iter().map(ResourceExt::name).collect::<Vec<_>>();
+            info!("Current crds: {:?}", crds);
+        }
+    });
+
+    let mut rfa = try_flatten_applied(rf).boxed();
+    while let Some(event) = rfa.try_next().await? {
+        info!("Changed {}", event.name());
     }
 
-    Err(anyhow!("them"))
+    Ok(())
 }
