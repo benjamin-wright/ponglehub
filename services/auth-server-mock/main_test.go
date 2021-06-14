69f293b0af19b7463caf07b203bbc5d0b5d51d71
Ben Wright <benjamin.p.wright@hotmail.com>
2021-06-14 07:50:52 +0100

    table-based tests

--- services/auth-server-mock/main_test.go
+++ services/auth-server-mock/main_test.go
@@ -12,18 +12,26 @@ import (
 )
 
 func TestDeleteRoute(t *testing.T) {
-	r := gin.Default()
-	s := state.New()
+	for _, test := range []struct {
+		name string
+	}{
+		{name: "works"},
+	} {
+		t.Run(test.name, func(u *testing.T) {
+			r := gin.Default()
+			s := state.New()
 
-	routes.RouteBuilder(r, s)
+			routes.RouteBuilder(r, s)
 
-	req, _ := http.NewRequest("GET", "/users", nil)
-	w := httptest.NewRecorder()
+			req, _ := http.NewRequest("GET", "/users", nil)
+			w := httptest.NewRecorder()
 
-	r.ServeHTTP(w, req)
+			r.ServeHTTP(w, req)
 
-	if w.Code != 200 {
-		fmt.Printf("Expected %d: Recieved %d\n", 200, w.Code)
-		t.Fail()
+			if w.Code != 200 {
+				fmt.Printf("Expected %d: Recieved %d\n", 200, w.Code)
+				u.Fail()
+			}
+		})
 	}
 }
