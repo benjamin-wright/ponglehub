513e86deaf5be0a80289613dc1fe6a7449a8e16d
Ben Wright <benjamin.p.wright@hotmail.com>
2021-06-14 07:48:06 +0100

    tests working

--- services/auth-server-mock/main_test.go
+++ services/auth-server-mock/main_test.go
@@ -5,18 +5,25 @@ import (
 	"net/http"
 	"net/http/httptest"
 	"testing"
+
+	"github.com/gin-gonic/gin"
+	"ponglehub.co.uk/auth-server-mock/internal/routes"
+	"ponglehub.co.uk/auth-server-mock/internal/state"
 )
 
 func TestDeleteRoute(t *testing.T) {
 	r := gin.Default()
+	s := state.New()
 
-	req, _ := http.NewRequest("DELETE", test.path, nil)
+	routes.RouteBuilder(r, s)
+
+	req, _ := http.NewRequest("GET", "/users", nil)
 	w := httptest.NewRecorder()
 
 	r.ServeHTTP(w, req)
 
-	if w.Code != test.code {
-		fmt.Printf("Expected %d: Recieved %d\n", test.code, w.Code)
-		u.Fail()
+	if w.Code != 200 {
+		fmt.Printf("Expected %d: Recieved %d\n", 200, w.Code)
+		t.Fail()
 	}
 }
