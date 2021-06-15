d65812a88c8c578fe070b2e7e6bde98585b737eb
Ben Wright <benjamin.wright@nielsen.com>
2021-06-15 16:34:50 +0100

    building up tests

--- services/auth-server-mock/go.mod
+++ services/auth-server-mock/go.mod
@@ -6,4 +6,5 @@ require (
 	github.com/gin-gonic/gin v1.7.1
 	github.com/google/uuid v1.2.0
 	github.com/sirupsen/logrus v1.8.1
+	github.com/stretchr/testify v1.4.0
 )
--- services/auth-server-mock/main_test.go
+++ services/auth-server-mock/main_test.go
@@ -1,25 +1,49 @@
 package main_test
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 	"net/http/httptest"
 	"testing"
 
 	"github.com/gin-gonic/gin"
+	"github.com/stretchr/testify/assert"
 	"ponglehub.co.uk/auth-server-mock/internal/routes"
 	"ponglehub.co.uk/auth-server-mock/internal/state"
 )
 
-func TestDeleteRoute(t *testing.T) {
+func toAnon(users []state.User) []map[string]interface{} {
+	output := []map[string]interface{}{}
+
+	for _, user := range users {
+		output = append(output, map[string]interface{}{
+			"id":       user.ID,
+			"name":     user.Name,
+			"email":    user.Email,
+			"verified": user.Verified,
+		})
+	}
+
+	return output
+}
+
+func TestListRoute(t *testing.T) {
+	user1 := state.User{ID: "some-id", Name: "ben", Email: "some@email.com", Password: "hashed", Verified: false}
+	user2 := state.User{ID: "some-other-id", Name: "geoss", Email: "other@email.com", Password: "alsohashed", Verified: false}
+
 	for _, test := range []struct {
-		name string
+		name     string
+		existing []state.User
 	}{
-		{name: "works"},
+		{name: "empty result", existing: []state.User{}},
+		{name: "one element", existing: []state.User{user1}},
+		{name: "two elements", existing: []state.User{user1, user2}},
 	} {
 		t.Run(test.name, func(u *testing.T) {
 			r := gin.Default()
 			s := state.New()
+			s.Users = test.existing
 
 			routes.RouteBuilder(r, s)
 
@@ -30,8 +54,17 @@ func TestDeleteRoute(t *testing.T) {
 
 			if w.Code != 200 {
 				fmt.Printf("Expected %d: Recieved %d\n", 200, w.Code)
-				u.Fail()
+				u.FailNow()
 			}
+
+			var response []map[string]interface{}
+			err := json.Unmarshal(w.Body.Bytes(), &response)
+			if err != nil {
+				fmt.Printf("Failed to parse json: %s", err.Error())
+				u.FailNow()
+			}
+
+			assert.EqualValues(u, toAnon(test.existing), response, "Expected list results to be the same")
 		})
 	}
 }
