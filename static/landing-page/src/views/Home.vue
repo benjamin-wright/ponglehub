b4a2736e3fd46e486341a67c2f13def2a6c54222
Ben Wright <benjamin.p.wright@hotmail.com>
2022-01-09 22:09:00 +0000

    adding landing page

---
+++ static/landing-page/src/views/Home.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="home">
+    <HelloWorld msg="Welcome to PongleHub Games!" />
+    <PongleFace />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from "@/components/HelloWorld.vue";
+import PongleFace from "@/components/PongleFace.vue";
+
+export default {
+  name: "Home",
+  components: {
+    HelloWorld,
+    PongleFace,
+  },
+};
+</script>
